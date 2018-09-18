17f9e28ad440c2e55f38307034a9f39706bba83d
Liam Barstad <liambarstad@gmail.com>
2018-09-18 13:26:01 -0600

    add illustrated circles

--- src/components/item-popout.js
+++ src/components/item-popout.js
@@ -1,6 +1,8 @@
 const closeIcon = require('../static/close-icon.png')
 const leftArrow = require('../static/arrow-left.png')
 const rightArrow = require('../static/arrow-right.png')
+const circleImg = require('../static/circle.svg')
+const selectedCircleImg = require('../static/selected-circle.svg')
 
 export default class ItemPopout {
   constructor(images) {
@@ -21,6 +23,7 @@ export default class ItemPopout {
     div.appendChild(this._closeIcon())
     div.appendChild(this._leftArrow())
     div.appendChild(this._rightArrow())
+    div.appendChild(this._bottomBar())
     return div
   }
 
@@ -59,6 +62,22 @@ export default class ItemPopout {
     rightImage.classList.add('popout-arrow')
     rightDiv.appendChild(rightImage)
     return rightDiv
+  }
+
+  _bottomBar() {
+    let bottomDiv = document.createElement('div')
+    bottomDiv.classList.add('popout-bottom-bar')
+    let selectedCircle = new Image()
+    selectedCircle.src = selectedCircleImg
+    selectedCircle.classList.add('popout-bottom-circle', 'selected-circle')
+    bottomDiv.appendChild(selectedCircle)
+    for (let i = 0; i < this.images.length - 1; i++) {
+      let selector = new Image()
+      selector.src = circleImg
+      selector.classList.add('popout-bottom-circle')
+      bottomDiv.appendChild(selector)
+    }
+    return bottomDiv
   }
 
   _parseHtmlImages() {
